0e9b4846a74218be3d5c0b7feaf0181f1c18af54
unknown <velualex10@gmail.com>
2024-04-22 17:42:41 -0600

    first commit

---
+++ controllers/paginasController.js
@@ -0,0 +1,90 @@
+
+import { Testimonial } from '../models/Testimoniales.js'
+import { Viaje } from '../models/Viaje.js'
+
+const paginaInicio =  async (req, res) => {
+
+    //Consultar 3 viajes
+
+    const promiseDB = []
+
+    promiseDB.push(Viaje.findAll({ limit: 3}))
+    promiseDB.push(Testimonial.findAll({ limit: 3}))
+
+    try {
+        const resultado = await Promise.all( promiseDB )
+
+        res.render('Inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes: resultado[0],
+            testimoniales: resultado[1]
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+const paginaNosotros = (req, res) => {
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    })
+}
+
+const paginaViajes = async (req, res) => {
+
+    //Consultar la BD
+    const viajes = await Viaje.findAll();
+
+    console.log(viajes);
+
+    res.render('viajes', {
+        pagina: 'Proximos Viajes',
+        viajes,
+    })
+}
+
+//Muestra un viaje por su slug
+const paginaDetalleViaje = async (req, res) => {
+
+    const { slug } = req.params;
+
+    try {
+        
+        const viaje = await Viaje.findOne({ where: {  slug }})
+        res.render('viaje',{
+            pagina: 'Información Viaje',
+            viaje
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const paginaTestimoniales =  async (req, res) => {
+
+    try {
+        const testimoniales = await Testimonial.findAll({ limit: 3});
+        
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            testimoniales
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+}
+
+export {
+    paginaInicio,
+    paginaNosotros,
+    paginaViajes,
+    paginaTestimoniales,
+    paginaDetalleViaje
+}
